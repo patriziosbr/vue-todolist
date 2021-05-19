7c71f0242057d8b36a3a1220cf6088b4652ebf02
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-05-19 14:52:32 +0200

    inizio lista

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
+// Creare con Vue una to-do list, in cui ogni elemento è un oggetto composto da:
+// - testo
+// - "completed", un booleano settato inizialmente a false.
+// Stampare in pagina un item per ogni elemento della to-do list.
+// Ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista.
+// Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure cliccando su un pulsante, il testo digitato viene aggiunto alla lista (all'interno di un nuovo oggetto con proprietà "completed" settata a false).
+// BONUS:
+// Al click sull'intero elemento della lista, si modifica il valore della proprietà completed da false a true, e viceversa in caso di successivi click (toggle).
+// Quando un elemento ha la proprietà "completed" settata a true, il testo corrispondente sarà mostrato barrato (esisterà una proprietà CSS per ottenere questo effetto???).
+
+
 var app = new Vue(
     {
     el: "#root",
     data: {
-       
-      },
-      methods: {
+       toDoList : [
+         {
+          text: "pane",
+          completed: false
+         },
+         {
+          text: "pasta",
+          completed: false
+         },
+         {
+          text: "pizza",
+          completed: false
+         },
+         {
+          text: "pesce",
+          completed: false
+         }
+       ]
+     },
+    methods: {
         
-      }
+    }
 
 } 
 );
